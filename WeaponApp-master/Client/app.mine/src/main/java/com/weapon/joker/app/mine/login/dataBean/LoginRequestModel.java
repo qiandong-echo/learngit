c06fa12e4a808ee102da88a93358991cfc18e996
qiandong-echo <723356385@qq.com>
2020-12-26 00:10:18 +0800

    message

---
+++ WeaponApp-master/Client/app.mine/src/main/java/com/weapon/joker/app/mine/login/dataBean/LoginRequestModel.java
@@ -0,0 +1,12 @@
+package com.weapon.joker.app.mine.login.dataBean;
+
+/**
+ * author : yueyang
+ * date : 2017/10/15
+ */
+public class LoginRequestModel {
+
+    public String name;
+    public String password;
+
+}
